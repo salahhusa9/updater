fa37d599b72d6455f5a604e9109a600638410304
salahhusa9 <salahhusa9@gmail.com>
2021-06-02 14:26:14 +0100

    change

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Project\Updater;
 
 Route::get('updater',[Updater::class,'view'])->name('updater');
 Route::get('updater/start/{mode?}',[Updater::class,'start'])->name('updater.start');
